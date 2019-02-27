406e9b525e304c9473ac080afbc24c7d0ed7b499
Aanisha Mishra <aanisha.mishra05@gmail.com>
2019-02-27 16:08:30 +0530

    Update Enormous Input Test.cpp

--- Enormous Input Test.cpp
+++ Enormous Input Test.cpp
@@ -1,3 +1,7 @@
+/***************************
+Programmer: Aanisha Mishra
+The question increments a counter if a number t is divisible by k, i.e. it leaves 0 as remainder
+***************************/
 #include<stdio.h>
 int main()
 {
