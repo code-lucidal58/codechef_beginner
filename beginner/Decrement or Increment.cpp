6e774fc3625ee14031c658024d8f1148e1d0ee22
code-lucidal58 <aanisha.mishra05@gmail.com>
2019-10-09 01:32:23 +0530

    added decrement or increment

---
+++ beginner/Decrement or Increment.cpp
@@ -0,0 +1,13 @@
+/***************************
+Programmer: Aanisha Mishra
+***************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	if(t%4==0) cout<<(t+1)<<endl;
+	else cout<<(t-1)<<endl;
+	return 0;
+}
